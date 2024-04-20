a278acf99b16255c5ebd59665bd926d13d51150b
estelaoli23 <estelaoliveirareato23@outlook.com>
2024-04-20 15:34:46 -0300

    feat:list1

---
+++ list/list_1.py
@@ -0,0 +1,11 @@
+# Criando Listas
+
+frutas = ["laranja","maçã", "uva"]
+
+frutas = []
+
+letras = list("python")
+
+numeros = list(range(10))
+
+carro = ["Ferrari", "F8", 42000, 2020, 2900, "São Paulo"]
